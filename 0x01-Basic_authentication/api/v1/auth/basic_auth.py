9566a5b5e8373eef78487aa4cac6f42261576875
Samuel Sylvester <sasy260902@gmail.com>
2024-09-04 03:17:18 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,9 @@
 BasicAuth module for API authentication.
 """
 import base64
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
-
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth. """
