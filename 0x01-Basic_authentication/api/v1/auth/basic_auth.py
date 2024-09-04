4d2d89f79a31290912f241e7e8762ff457a9760d
Samuel Sylvester <sasy260902@gmail.com>
2024-09-04 03:06:54 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,5 +53,6 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        usr_email, usr_password = decoded_base64_authorization_header.split(':', 1)
+        usr_email, usr_password =
+               decoded_base64_authorization_header.split(':', 1)
         return usr_email, usr_password
