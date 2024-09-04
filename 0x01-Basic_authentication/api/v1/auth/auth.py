05bcd9d2f1338451cfb0336a61a118ad6103620d
Samuel Sylvester <sasy260902@gmail.com>
2024-09-04 02:18:25 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,6 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required for a given path.
-        
-        Args:
-            path (str): The requested path.
-            excluded_paths (List[str]): A list of paths that do not require authentication.
-        
         Returns:
             bool: False for now. (Logic to be implemented later)
         """
@@ -24,10 +19,6 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """
         Retrieves the Authorization header from the request object.
-        
-        Args:
-            request: The Flask request object (optional).
-        
         Returns:
             str: None for now.
         """
@@ -36,10 +27,6 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """
         Retrieves the current user based on the request.
-        
-        Args:
-            request: The Flask request object (optional).
-        
         Returns:
             TypeVar('User'): None for now.
         """
