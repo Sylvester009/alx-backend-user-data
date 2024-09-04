9f22401fc9dcc69a909bcc0dd5caa5938cc8d5aa
Samuel Sylvester <sasy260902@gmail.com>
2024-09-04 02:57:26 +0100

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth module for API authentication.
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -9,7 +10,7 @@ class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth. """
 
     def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+           self, authorization_header: str) -> str:
         """
         Extracts the Base64 part of the Authorization
         header for Basic Authentication.
@@ -22,3 +23,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+
+    def decode_base64_authorization_header(
+           self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            base64_byte = base64.b64decode(base64_authorization_header)
+            return base64_byte.decode('utf-8')
+        except (TypeError, base64.binascii.Error, UnicodeDecodeError):
+            return None
