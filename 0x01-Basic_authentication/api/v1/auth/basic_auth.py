cd1aeaa8d0098c1454756555a44855a6bdfdfdb3
Samuel Sylvester <sasy260902@gmail.com>
2024-09-04 03:14:34 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,3 +56,22 @@ class BasicAuth(Auth):
         usr_email, usr_password = decoded_base64_authorization_header.split(':', 1)
 
         return usr_email, usr_password
+
+    def user_object_from_credentials(
+           self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password.
+        """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+
+        user = User.search(user_email)
+        if not user:
+            return None
+
+        if not user.is_valid_password(user_pwd):
+            return None
+        
+        return user
