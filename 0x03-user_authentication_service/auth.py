78236d8259458b24437c6cfd8104495a1613dd88
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-13 00:05:30 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,12 @@
 login and password management.
 """
 import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
