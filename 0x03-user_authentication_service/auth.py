12f30f74e4a83abf6a2e07e6d08230108bf86f77
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-13 00:20:24 +0100

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,11 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+def _generate_uuid() -> str:
+    """Generate unique UUID
+    """
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
